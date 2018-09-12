95c02347902ba2800605ee46be3b917dd3f00fe9
Clint Jang <jang.wangsu@gmail.com>
2018-09-12 10:02:09 +0900

    Code style : 스토어 생성 관련

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import reducers from '../reducers';
 import { createStore } from 'redux';
 
 export default function initStore() {
-    const store = createStore( reducers );
+    const store = createStore( 
+        reducers,
+        applyMiddleware(
+            // Middleware will not be applied to this sample.
+        ), 
+    );
     return store;
 }
